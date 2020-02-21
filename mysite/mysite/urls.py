96d2596f3b7e1fd98d32c7079f5151dc059c950c
navneet kumar singh <>
2020-02-21 16:09:11 +0530

    Final Version

---
+++ mysite/mysite/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import include, path
+from django.views.generic import TemplateView
+urlpatterns = [
+    path('polls/', include('polls.urls')),
+    path('admin/', admin.site.urls),
+    path('accounts/',include('allauth.urls')),
+    path('',TemplateView.as_view(template_name="login/index.html")),
+
+]
