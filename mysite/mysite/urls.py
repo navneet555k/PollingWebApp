b758483a4c191a0ecc4ad45bd9cd5720ef6f7b81
navneet kumar singh <>
2020-03-03 03:05:49 +0530

    created new poll

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/',include('allauth.urls')),
     path('',TemplateView.as_view(template_name="login/index.html")),
-
 ]
--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -5,7 +5,7 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
     def __str__(self):
-        return self.question_text
+        return self.question_text 
 
 
 class Choice(models.Model):
